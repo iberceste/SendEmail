20fc83d098337811a4364106830bf2562ae042c8
İpek Ünlü <ipekunlu18@gmail.com>
2024-02-01 15:30:20 +0300

    Send a motivational quote in an email
    Send a motivational quote via email on the current weekday (You can change it to any day you want)

---
+++ main.py
@@ -0,0 +1,27 @@
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+
+my_email = "ipekkunluupy@gmail.com"
+password = "your_password"
+now = dt.datetime.now()
+day = now.weekday()
+
+with open("quotes.txt") as f:
+    lines = f.readlines()
+    choose_quote = random.choice(lines)
+
+if day == 3: #You can change it to any day you want
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="ipekkunluupy@yahoo.com",
+                            msg=f"Subject:Daily Quote\n\n{choose_quote}")
+        connection.close()
+
+
+
+
